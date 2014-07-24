daa0cab8f32055b83bd7590cc7be7fb676e2df07
konglingjie <547518762@qq.com>
2014-07-24 10:31:41 +0800

    MyMvc

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "Your News page.";
+            return View();
+        }
+
+        public ActionResult SuccessStories()
+        {
+            ViewBag.Message = "Your SuccessStories Page";
+            return View();
+        }
+
+        public ActionResult Custom()
+        {
+            ViewBag.Message = "Your Custom page";
+            return View();
+        }
+
+        public ActionResult Message()
+        {
+            ViewBag.Message = "Your Message page";
+            return View();
+        }
+    }
+}
